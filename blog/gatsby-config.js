1e0bd08828923dd4738d584940cebe6e70f9f67e
Jouliet Morales <jouliet.25@gmai.com>
2021-03-31 14:28:35 -0500

    merge activity 7

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,48 +1,48 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
-});
+// Initialize dotenv
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {
-    title: `SquareSpaces`,
-    description: `Testing Project`,
-    author: `Jouliet Morales`,
+    title: "Cool Blog",
+    description: "ITDEV-164 Gatsby Blog.",
+    author: "Jouliet Morales",
     contact: {
-      name: `Jouliet Morales`,
-      company: `SquareSpaces Inc.`,
-      address: `PO Box 1234`,
+      name: "Jouliet Morales",
+      company: "Blogs Inc.",
+      address: "PO Box 1234",
     },
   },
   plugins: [
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: "gatsby-source-contentful",
       options: {
         spaceId: `${process.env.SPACE_ID}`,
         accessToken: `${process.env.ACCESS_TOKEN}`,
       },
     },
+    "gatsby-plugin-styled-components",
     "gatsby-transformer-remark",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-styled-components",
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     "gatsby-plugin-image",
+
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png",
       },
     },
-    // `gatsby-plugin-gatsby-cloud`,
-
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    //"gatsby-plugin-`offline`
+    "gatsby-plugin-offline",
   ],
 }
